abd54b33310058038d74bc3ed12d461eca3e9e3e
Oluwatobi Onasanya <onatty98@gmail.com>
2019-01-25 04:11:46 +0100

    first set

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import React from 'react'
+import MessageList from './components/MessageList'
+import SendMessageForm from './components/SendMessageForm'
+import RoomList from './components/RoomList'
+import NewRoomForm from './components/NewRoomForm'
+import Chatkit from '@pusher/chatkit-client'
+import { tokenUrl, instanceLocator } from './config'
+class App extends React.Component {
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+        instanceLocator,
+        userId: 'perborgen',
+        tokenProvider: new Chatkit.TokenProvider({
+            url: tokenUrl
+        })
+    })
+    
+    chatManager.connect()
+    .then(currentUser => {
+        currentUser.subscribeToRoom({
+            roomId: 9434230,
+            hooks: {
+                onNewMessage: message => {
+                    console.log('message.text: ', message.text);
+                }
+            }
+        })
+    })
 }
 
-export default App;
+    render() {
+        return (
+            <div className="app">
+                <RoomList />
+                <MessageList />
+                <SendMessageForm />
+                <NewRoomForm />
+            </div>
+        );
+    }
+}
+
+export default App
---
+++ src/config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/1d5e00fe-40b7-4c08-a548-e2400f4c0855/token";
+const instanceLocator = "v1:us1:1d5e00fe-40b7-4c08-a548-e2400f4c0855";
+
+export { tokenUrl, instanceLocator }
