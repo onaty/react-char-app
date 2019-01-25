c421b07d9d91570ee76f7c6e7d9b847a30d4ab2b
Oluwatobi Onasanya <onatty98@gmail.com>
2019-01-25 06:06:54 +0100

    chat app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
 import React from 'react'
+import Chatkit from '@pusher/chatkit-client'
 import MessageList from './components/MessageList'
 import SendMessageForm from './components/SendMessageForm'
 import RoomList from './components/RoomList'
 import NewRoomForm from './components/NewRoomForm'
-import Chatkit from '@pusher/chatkit-client'
+
 import { tokenUrl, instanceLocator } from './config'
+
 class App extends React.Component {
-   
-      state = {
-            messages: []
-      }
-  componentDidMount() {
-    const chatManager = new Chatkit.ChatManager({
-        instanceLocator,
-        userId: 'femi',
-        tokenProvider: new Chatkit.TokenProvider({
-            url: tokenUrl
-        })
-    })
     
-    chatManager.connect()
-    .then(currentUser => {
-        currentUser.subscribeToRoom({
-            roomId: '19378175',
+    constructor() {
+        super()
+        this.state = {
+            roomId: null,
+            messages: [],
+            joinableRooms: [],
+            joinedRooms: []
+        }
+        this.sendMessage = this.sendMessage.bind(this)
+        this.subscribeToRoom = this.subscribeToRoom.bind(this)
+        this.getRooms = this.getRooms.bind(this)
+        this.createRoom = this.createRoom.bind(this)
+    } 
+    
+    componentDidMount() {
+        const chatManager = new Chatkit.ChatManager({
+            instanceLocator,
+            userId: 'femi',
+            tokenProvider: new Chatkit.TokenProvider({
+                url: tokenUrl
+            })
+        })
+        
+        chatManager.connect()
+        .then(currentUser => {
+            this.currentUser = currentUser
+            this.getRooms()
+          
+        })
+        .catch(err => console.log('error on connecting: ', err))
+
+    }
+
+    getRooms() {
+        this.currentUser.getJoinableRooms()
+        .then(joinableRooms => {
+            this.setState({
+                joinableRooms,
+                joinedRooms: this.currentUser.rooms
+            })
+        })
+        .catch(err => console.log('error on joinableRooms: ', err))
+    }
+
+    subscribeToRoom(roomId) {
+        this.setState({ messages: [] })
+        this.currentUser.subscribeToRoom({
+            roomId: roomId.toString(),
             hooks: {
                 onMessage: message => {
                     this.setState({
@@ -31,19 +65,49 @@ class App extends React.Component {
                 }
             }
         })
-    })
-}
+        .then(room => {
+            this.setState({
+                roomId: room.id.toString()
+            })
+            this.getRooms()
+        })
+        .catch(err => console.log('error on subscribing to room: ', err))
+    }
+    
+    sendMessage(text) {
+        this.currentUser.sendMessage({
+            text,
+            roomId: '19378175'
+        })
+    }
 
+    createRoom(name) {
+        this.currentUser.createRoom({
+            name
+        })
+        .then(room => this.subscribeToRoom(room.id))
+        .catch(err => console.log('error with createRoom: ', err))
+    }
+    
+    
     render() {
         return (
             <div className="app">
-                <RoomList />
-                <MessageList messages={this.state.messages} />
-                <SendMessageForm />
-                <NewRoomForm />
+            <RoomList
+                    subscribeToRoom={this.subscribeToRoom}
+                    rooms={[...this.state.joinableRooms, ...this.state.joinedRooms]}
+                    roomId={this.state.roomId} />
+                <MessageList 
+                    roomId={this.state.roomId}
+                    messages={this.state.messages} />
+                <SendMessageForm
+                    disabled={!this.state.roomId}
+                    sendMessage={this.sendMessage} />
+                <NewRoomForm createRoom={this.createRoom} />
             </div>
         );
     }
 }
+
 
 export default App
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,17 +1,36 @@
 import React from 'react'
-
-
+import ReactDOM from 'react-dom'
+import Message from './Message'
 
 class MessageList extends React.Component {
+    
+    componentWillUpdate() {
+        const node = ReactDOM.findDOMNode(this)
+        this.shouldScrollToBottom = node.scrollTop + node.clientHeight + 100 >= node.scrollHeight
+    }
+    
+    componentDidUpdate() {
+        if (this.shouldScrollToBottom) {
+            const node = ReactDOM.findDOMNode(this)
+            node.scrollTop = node.scrollHeight   
+        }
+    }
+    
     render() {
+        if (!this.props.roomId) {
+            return (
+                <div className="message-list">
+                    <div className="join-room">
+                        &larr; Join a room!
+                    </div>
+                </div>
+            )
+        }
         return (
             <div className="message-list">
-            {this.props.messages.map((message, index) => {
+                {this.props.messages.map((message, index) => {
                     return (
-                        <div key={index} className="message">
-                            <div className="message-username">{message.senderId}</div>
-                            <div className="message-text">{message.text}</div>
-                        </div>
+                        <Message key={message.id} username={message.senderId} text={message.text} />
                     )
                 })}
             </div>
